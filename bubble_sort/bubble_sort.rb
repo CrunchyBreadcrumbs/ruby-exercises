7d1d542198efb780287898998ecc7ee404c98648
CrunchyBreadcrumbs <144739569+CrunchyBreadcrumbs@users.noreply.github.com>
2024-08-16 10:19:26 +0200

    Create bubble_sort.rb

---
+++ bubble_sort/bubble_sort.rb
@@ -0,0 +1,20 @@
+def bubble_sort(array)
+  n = array.length
+
+  until n <= 1
+    newn = 0
+    array[1..n].each_with_index do |number, index|
+      j = index + 1
+      if array[j - 1] > array[j]
+        array[j], array[j - 1] = array[j - 1], array[j]
+        newn = j
+      end
+    end
+    n = newn
+  end
+p array
+end
+
+bubble_sort([4,3,78,2,0,2])
+bubble_sort([4,3,78,4,5,1,3,2,0,2])
+bubble_sort([1,4,3,78,0,2,0,2,45,8,854])
